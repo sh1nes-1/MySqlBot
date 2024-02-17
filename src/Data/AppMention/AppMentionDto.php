270d76a1e35319230ef20b5fe9516cffd38cd61d
Serhiy Kosovchych <kosovchychserhiy@gmail.com>
2024-02-17 17:04:40 +0200

    Finish AppMentionDto

--- src/Data/AppMention/AppMentionDto.php
+++ src/Data/AppMention/AppMentionDto.php
@@ -2,6 +2,9 @@
 
 namespace Sh1ne\MySqlBot\Data\AppMention;
 
+/**
+ * @link https://api.slack.com/events/app_mention
+ */
 class AppMentionDto
 {
 
@@ -10,8 +13,14 @@ class AppMentionDto
      */
     public readonly string $token;
 
+    /**
+     * @var string $teamId Example: T123ABC456
+     */
     public readonly string $teamId;
 
+    /**
+     * @var string $apiAppId Example: A123ABC456
+     */
     public readonly string $apiAppId;
 
     public readonly AppMentionEventDto $event;
@@ -21,18 +30,31 @@ class AppMentionDto
      */
     public readonly string $type;
 
+    /**
+     * @var string $eventId Example: Ev123ABC456
+     */
     public readonly string $eventId;
 
+    /**
+     * @var string $eventTime Example: 1515449522000016
+     */
     public readonly string $eventTime;
 
     /**
-     * @var array<string> $authedUsers
+     * @var array<string> $authedUsers Example: ["U0LAN0Z89"]
      */
     public readonly array $authedUsers;
 
     public function __construct(array $data)
     {
         $this->token = $data['token'];
+        $this->teamId = $data['team_id'];
+        $this->apiAppId = $data['api_app_id'];
+        $this->event = new AppMentionEventDto($data['event']);
+        $this->type = $data['type'];
+        $this->eventId = $data['event_id'];
+        $this->eventTime = $data['event_time'];
+        $this->authedUsers = $data['authed_users'];
     }
 
 }
--- src/Data/AppMention/AppMentionEventDto.php
+++ src/Data/AppMention/AppMentionEventDto.php
@@ -2,24 +2,50 @@
 
 namespace Sh1ne\MySqlBot\Data\AppMention;
 
+/**
+ * @see AppMentionDto
+ */
 class AppMentionEventDto
 {
 
+    /**
+     * @var string $type Example: app_mention
+     */
     public readonly string $type;
 
+    /**
+     * @var string $user Example: U123ABC456
+     */
     public readonly string $user;
 
+    /**
+     * @var string $text Example: What is the hour of the pearl, <@U0LAN0Z89>?
+     */
     public readonly string $text;
 
+    /**
+     * @var string $ts Example: 1515449522.000016
+     */
     public readonly string $ts;
 
+    /**
+     * @var string $channel Example: C123ABC456
+     */
     public readonly string $channel;
 
+    /**
+     * @var string $eventTs Example: 1515449522000016
+     */
     public readonly string $eventTs;
 
     public function __construct(array $data)
     {
-        // TODO
+        $this->type = $data['type'];
+        $this->user = $data['user'];
+        $this->text = $data['text'];
+        $this->ts = $data['ts'];
+        $this->channel = $data['channel'];
+        $this->eventTs = $data['event_ts'];
     }
 
 }
